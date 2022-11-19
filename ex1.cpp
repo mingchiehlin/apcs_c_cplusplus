cc6893b18b256a484f6f5f7480a729f3b81fc74c
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-18 20:48:44 -0700

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -2,10 +2,10 @@
 int main (void) {
      printf(" hello mclin! "); 
      printf(" my height is %d m %d cm \n",1,67);
-     printf(" this building is %5d cm \n", 890);
-     printf(" this building is %5d cm \n", 7890);
-     printf(" this building is %5d cm \n", 67890);
-     printf(" this building is %5d cm \n", 567890);
-     printf(" this building is %5d cm \n", 1234567890);
+     printf(" this building is %10d cm \n", 890);
+     printf(" this building is %10d cm \n", 7890);
+     printf(" this building is %10d cm \n", 67890);
+     printf(" this building is %10d cm \n", 567890);
+     printf(" this building is %10d cm \n", 1234567890);
      printf(" pi = %2.16f \n ", 3.1415926535897932384626433832795);
 }
