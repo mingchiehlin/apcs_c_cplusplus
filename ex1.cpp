1fbb0175ee3896f697b221a1911353f2b25a1262
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-18 20:46:46 -0700

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void) {
+     printf(" hello mclin! "); 
+     printf(" my height is %d m %d cm \n",1,67);
+     printf(" this building is %5d cm \n", 890);
+     printf(" this building is %5d cm \n", 7890);
+     printf(" this building is %5d cm \n", 67890);
+     printf(" this building is %5d cm \n", 567890);
+     printf(" this building is %5d cm \n", 1234567890);
+     printf(" pi = %2.16f \n ", 3.1415926535897932384626433832795);
+}
