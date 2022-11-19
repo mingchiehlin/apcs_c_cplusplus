ff95c2eef85d18b95c7c4e80592ab757271e9549
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-18 22:58:48 -0700

    Create ex5.cpp

---
+++ ex5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("A=>");
+    scanf("%d", &a);
+    printf("B=>");
+    scanf("%d", &b);
+    printf("A + B=%d \n", a+b);
+    printf("A - B=%d \n", a-b);
+    printf("A * B=%d \n", a*b);
+    printf("A / B=%d \n", a/b);
+    printf("A % B=%d \n", a%b);
+    return 0;
+}
