895b7d95bdf1ddf0626c3ef8916b8fa8d0b652a7
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-19 00:41:42 -0700

    Create ex9.cpp

---
+++ ex9.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void){
+	int x,y; 
+	int bool_value;
+	
+	x = 3 ; y= 4; 
+	
+	bool_value = ( x > y );
+	printf(" x > y ==> %d \n ",bool_value);
+	bool_value = ( x < y );
+	printf(" x < y ==> %d \n ",bool_value);	
+	bool_value = (x < 5 && x < 10);	
+	printf(" x < 5 &&  x < 10 ==> %d \n ",bool_value);	
+	bool_value = (x < 5 || x > 10);	
+	printf(" x < 5 ||  x > 10 ==> %d \n ",bool_value);	
+	bool_value = !(x < 5 || x > 10);	
+	printf(" !(x < 5 ||  x > 10) ==> %d \n ",bool_value);	
+}
