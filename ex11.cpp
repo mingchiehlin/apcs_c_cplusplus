64d7e4d6a012f29c653345b5f69edc6cc3adcdb9
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-19 01:16:12 -0700

    Create ex11.cpp

---
+++ ex11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	printf("please inout a number: \n");
+	scanf("%d", &a);
+	printf("please inout another number: \n");
+	scanf("%d", &b);
+	if (a>b){
+		printf("a > b \n");
+	}else if (a < b){
+		printf("a < b \n");
+	}else{
+	}
+	cout<<"Hello World!";
+	return 0;
+}
