a710b71c4dae7998ff2d25e318d2b53acec19fda
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-19 19:44:22 -0700

    Create ex15.cpp

---
+++ ex15.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    printf("Please enter the year and the month: ");
+    scanf("%d %d", &i, &j);
+if (i%4==0){
+    printf("It is a Leap Year! \n");
+    if ((j==1)||(j==3)||(j==5)||(j==7)||(j==8)||(j==10)||(j==12)){
+    printf("The number of days in the month is 31.");
+    }
+    else if ((j==4)||(j==6)||(j==9)||(j==11)){
+    printf("The number of days in the month is 30.");
+    }else {
+    printf("The number of days in the month is 29.");
+    }
+    }    
+else{
+    printf("It is not a Leap Year! \n");
+        if ((j==1)||(j==3)||(j==5)||(j==7)||(j==8)||(j==10)||(j==12)){
+    printf("The number of days in the month is 31.");
+    }
+    else if ((j==4)||(j==6)||(j==9)||(j==11)){
+    printf("The number of days in the month is 30.");
+    }else {
+    printf("The number of days in the month is 28.");
+    }
+    }
+    return 0;
+}
