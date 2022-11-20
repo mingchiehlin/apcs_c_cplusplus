9ab7737960de5c710425d5cc8ccd95684f8ed8fe
mingchiehlin <118579103+mingchiehlin@users.noreply.github.com>
2022-11-19 18:45:44 -0700

    Update ex12.cpp

--- ex12.cpp
+++ ex12.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-	double r, area;
+	double r, area, circumference;
 	printf("please input a radius: \n");
 	scanf("%lf", &r);
 	printf("The radius is %lf \n", r);
 	if (r >= 0.0){
 	    area=3.14159*r*r;
+	    circumference=2*3.14159*r;
 		printf("The area of the circle is %lf \n", area);
+		printf("The circumference of the circle is %lf \n",  circumference);
 	}else {
 		printf("A radius should be greater than 0! \n");
 	}
